f3aa336145dd0717b76e7e0f3a2ddf6e4a7f2706
Eric Wooley <ericwooley@gmail.com>
2017-07-22 12:58:17 -0700

    fixed tests, broke storybook

---
+++ combined/App.test-m.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import App from './App';
+import {shallow} from 'enzyme'
+it('renders without crashing', () => {
+  const rendered = shallow(<App />)
+  expect(rendered.length).toBe(1)
+});
